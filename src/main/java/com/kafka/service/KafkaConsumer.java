4a95e330657686b320c6c229ca01fb7ad71d1d93
rasel-machette <md.rasel@machette.tech>
2021-09-24 23:35:04 +0600

    first commit

---
+++ src/main/java/com/kafka/service/KafkaConsumer.java
@@ -0,0 +1,26 @@
+package com.kafka.service;
+
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.kafka.model.User;
+
+@Service
+public class KafkaConsumer {
+
+ // @KafkaListener(topics = "Kafka_Example", groupId = "group_id")
+   @KafkaListener(topics = "User_Rasel", groupId = "group_id")
+    public void consume(String message) {
+        System.out.println("Consumed message: " + message);
+    }
+
+
+//   // @KafkaListener(topics = "Kafka_Example_json", groupId = "group_json",
+//           // containerFactory = "userKafkaListenerFactory")
+//   @KafkaListener(topics = "Rasel", groupId = "group_json",
+//   containerFactory = "userKafkaListenerFactory")
+//    public void consumeJson(User user) {
+//        System.out.println("Consumed JSON Message: " + user);
+//    }
+}
